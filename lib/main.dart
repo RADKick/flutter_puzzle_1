8181c13220489ed580f46d66a8f8c07c824c470c
krisha <krishapahwa@gmail.com>
2020-09-07 16:34:01 -0400

    Merge branch 'master' of C:\Users\krish\StudioProjects\flutter_puzzle_1 with conflicts.

--- lib/main.dart
+++ lib/main.dart
@@ -8,15 +8,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Test"),
-        ),
-        body: Center(
-          child: Text("Hello World"),
-        ),
-      ),
-    );
+    return MaterialApp();
   }
 }
+
